44d5661ee739ac9c97861c988152d188347f14e1
Wojciech Kielak <wojtekkielak@gmail.com>
2023-09-30 10:03:39 +0200

    mod proc rap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModelsComponent } from './models/models.component';
+import { ProcessesComponent } from './processes/processes.component';
+import { ReportComponent } from './report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'models', component: ModelsComponent},
+  // { path:'report', component: ReportComponent},
+  { path:'**',component: ProcessesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/processes/processes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProcessesComponent } from './processes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProcessesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProcessesModule { }
