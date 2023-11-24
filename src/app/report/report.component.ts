3e0e4d8cbef68bf86db064f539ad03584e958df4
Wojciech Kielak <wojtekkielak@gmail.com>
2023-11-24 21:47:05 +0100

    raporty - wyswietlanie tabeli

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -6,26 +6,6 @@ import { EventReportList } from './eventReport';
 import { DataSource } from '@angular/cdk/table';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 
 @Component({
   selector: 'app-report',
@@ -33,8 +13,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit{
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = [ 'name', 'savg', 'max_time', 'median', 'min_time', 'number_of_employee', 'sum',
+   'tasks_execute', 'tasks_pending', 'tasks_realized'];
+
  // dataSource = new MatTableDataSource(ELEMENT_DATA);
   constructor(private reportService: ReportService, private route: ActivatedRoute,
     private router: Router){}
